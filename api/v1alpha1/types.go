244bb2a4629fd426b4f5167eeb2bc8b97b6fe059
Jaroslav Viravec <jaroslav.viravec@sap.com>
2025-01-23 11:57:56 +0100

    Initial valkey-operator created

---
+++ api/v1alpha1/types.go
@@ -0,0 +1,158 @@
+/*
+Copyright 2025 SAP SE.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+
+	prometheusv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	"github.com/sap/component-operator-runtime/pkg/component"
+	componentoperatorruntimetypes "github.com/sap/component-operator-runtime/pkg/types"
+)
+
+// ValkeySpec defines the desired state of Valkey.
+type ValkeySpec struct {
+	Version string `json:"version,omitempty"`
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:default=1
+	Replicas                                int `json:"replicas,omitempty"`
+	component.KubernetesPodProperties       `json:",inline"`
+	component.KubernetesContainerProperties `json:",inline"`
+	Sidecars                                []corev1.Container     `json:"sidecars,omitempty"`
+	Sentinel                                *SentinelProperties    `json:"sentinel,omitempty"`
+	Metrics                                 *MetricsProperties     `json:"metrics,omitempty"`
+	TLS                                     *TLSProperties         `json:"tls,omitempty"`
+	Persistence                             *PersistenceProperties `json:"persistence,omitempty"`
+	Binding                                 *BindingProperties     `json:"binding,omitempty"`
+}
+
+// SentinelProperties models attributes of the sentinel sidecar
+type SentinelProperties struct {
+	Enabled                                 bool `json:"enabled,omitempty"`
+	component.KubernetesContainerProperties `json:",inline"`
+}
+
+// MetricsProperties models attributes of the metrics exporter sidecar
+type MetricsProperties struct {
+	Enabled                                 bool `json:"enabled,omitempty"`
+	component.KubernetesContainerProperties `json:",inline"`
+	ServiceMonitor                          *MetricsServiceMonitorProperties `json:"monitor,omitempty"`
+	PrometheusRule                          *MetricsPrometheusRuleProperties `json:"prometheusRule,omitempty"`
+}
+
+type MetricsServiceMonitorProperties struct {
+	Enabled            bool                         `json:"enabled,omitempty"`
+	Interval           prometheusv1.Duration        `json:"interval,omitempty"`
+	ScrapeTimeout      prometheusv1.Duration        `json:"scrapeTimeout,omitempty"`
+	Relabellings       []prometheusv1.RelabelConfig `json:"relabellings,omitempty"`
+	MetricRelabellings []prometheusv1.RelabelConfig `json:"metricRelabelings,omitempty"`
+	HonorLabels        bool                         `json:"honorLabels,omitempty"`
+	AdditionalLabels   map[string]string            `json:"additionalLabels,omitempty"`
+	PodTargetLabels    []string                     `json:"podTargetLabels,omitempty"`
+}
+
+type MetricsPrometheusRuleProperties struct {
+	Enabled          bool                `json:"enabled,omitempty"`
+	AdditionalLabels map[string]string   `json:"additionalLabels,omitempty"`
+	Rules            []prometheusv1.Rule `json:"rules,omitempty"`
+}
+
+// TLSProperties models TLS settings of the redis services
+type TLSProperties struct {
+	Enabled     bool                   `json:"enabled,omitempty"`
+	CertManager *CertManagerProperties `json:"certManager,omitempty"`
+}
+
+// CertManagerProperties models cert-manager related attributes
+type CertManagerProperties struct {
+	Issuer *ObjectReference `json:"issuer,omitempty"`
+}
+
+// ObjectReference models a reference to a Kubernetes object
+type ObjectReference struct {
+	Group string `json:"group,omitempty"`
+	Kind  string `json:"kind,omitempty"`
+	Name  string `json:"name,omitempty"`
+}
+
+// PersistenceProperties models persistence related attributes
+type PersistenceProperties struct {
+	Enabled      bool               `json:"enabled,omitempty"`
+	Size         *resource.Quantity `json:"size,omitempty"`
+	StorageClass string             `json:"storageClass,omitempty"`
+	ExtraVolumes []corev1.Volume    `json:"extraVolumes,omitempty"`
+}
+
+// BindingProperties models custom properties for the generated binding secret
+type BindingProperties struct {
+	Template *string `json:"template,omitempty"`
+}
+
+// ValkeyStatus defines the observed state of Valkey.
+type ValkeyStatus struct {
+	component.Status `json:",inline"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="State",type=string,JSONPath=`.status.state`
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+// +genclient
+
+// Valkey is the Schema for the valkeies API.
+type Valkey struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec ValkeySpec `json:"spec,omitempty"`
+	// +kubebuilder:default={"observedGeneration":-1}
+	Status ValkeyStatus `json:"status,omitempty"`
+}
+
+var _ component.Component = &Valkey{}
+
+// +kubebuilder:object:root=true
+
+// ValkeyList contains a list of Valkey.
+type ValkeyList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Valkey `json:"items"`
+}
+
+func (s *ValkeySpec) ToUnstructured() map[string]any {
+	result, err := runtime.DefaultUnstructuredConverter.ToUnstructured(s)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func (c *Valkey) GetSpec() componentoperatorruntimetypes.Unstructurable {
+	return &c.Spec
+}
+
+func (c *Valkey) GetStatus() *component.Status {
+	return &c.Status.Status
+}
+
+func init() {
+	SchemeBuilder.Register(&Valkey{}, &ValkeyList{})
+}
---
+++ main.go
@@ -0,0 +1,148 @@
+/*
+Copyright 2025 SAP SE.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"flag"
+	"net"
+	"os"
+	"strconv"
+
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	apiregistrationv1 "k8s.io/kube-aggregator/pkg/apis/apiregistration/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+
+	"github.com/sap/valkey-operator/pkg/operator"
+)
+
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(apiextensionsv1.AddToScheme(scheme))
+	utilruntime.Must(apiregistrationv1.AddToScheme(scheme))
+
+	operator.InitScheme(scheme)
+}
+
+func main() {
+	var metricsAddr string
+	var probeAddr string
+	var webhookAddr string
+	var webhookCertDir string
+	var enableLeaderElection bool
+
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080",
+		"The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081",
+		"The address the probe endpoint binds to.")
+	flag.StringVar(&webhookAddr, "webhook-bind-address", ":2443",
+		"The address the webhooks endpoint binds to.")
+	flag.StringVar(&webhookCertDir, "webhook-tls-directory", "",
+		"The directory containing tls server key and certificate, as tls.key and tls.crt; defaults to $TMPDIR/k8s-webhook-server/serving-certs")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+	operator.InitFlags(flag.CommandLine)
+	opts := zap.Options{
+		Development: false,
+	}
+	opts.BindFlags(flag.CommandLine)
+	flag.Parse()
+
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	if err := operator.ValidateFlags(); err != nil {
+		setupLog.Error(err, "error validating command line flags")
+		os.Exit(1)
+	}
+
+	webhookHost, webhookPort, err := parseAddress(webhookAddr)
+	if err != nil {
+		setupLog.Error(err, "error parsing webhook address")
+		os.Exit(1)
+	}
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme: scheme,
+		Client: client.Options{
+			Cache: &client.CacheOptions{
+				DisableFor: append(operator.GetUncacheableTypes(), &apiextensionsv1.CustomResourceDefinition{}, &apiregistrationv1.APIService{}),
+			},
+		},
+		LeaderElection:                enableLeaderElection,
+		LeaderElectionID:              operator.GetName(),
+		LeaderElectionReleaseOnCancel: true,
+		WebhookServer: webhook.NewServer(webhook.Options{
+			Host:    webhookHost,
+			Port:    webhookPort,
+			CertDir: webhookCertDir,
+		}),
+		Metrics: metricsserver.Options{
+			BindAddress: metricsAddr,
+		},
+		HealthProbeBindAddress: probeAddr,
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	if err := operator.Setup(mgr); err != nil {
+		setupLog.Error(err, "error registering controller with manager")
+		os.Exit(1)
+	}
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}
+
+func parseAddress(address string) (string, int, error) {
+	host, p, err := net.SplitHostPort(address)
+	if err != nil {
+		return "", -1, err
+	}
+	port, err := strconv.Atoi(p)
+	if err != nil {
+		return "", -1, err
+	}
+	return host, port, nil
+}
