c033fc09110478b5ee7ae0676a567a2f68f103b1
Daniel Ferenci <daniel.ferenci@sap.com>
2025-06-30 17:27:18 +0200

    Enable extra flags

--- api/v1alpha1/types.go
+++ api/v1alpha1/types.go
@@ -31,6 +31,7 @@ type ValkeySpec struct {
 	Persistence                             *PersistenceProperties `json:"persistence,omitempty"`
 	Binding                                 *BindingProperties     `json:"binding,omitempty"`
 	ExtraEnvVars                            []corev1.EnvVar        `json:"extraEnvVars,omitempty"`
+	ExtraFlags                              []string               `json:"extraFlags,omitempty"`
 }
 
 // SentinelProperties models attributes of the sentinel sidecar
